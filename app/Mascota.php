e5713dd5b2720a9963deb67b19fe48b5533cc061
BrayanMaychuc <brayanmay137@gmail.com>
2021-10-14 15:27:02 -0500

    Modelo Raza
    Creación del modelo y establecer su relación

--- app/Mascota.php
+++ app/Mascota.php
@@ -12,6 +12,7 @@ class Mascota extends Model
     //especificamos las relaciones
 
     public $with=['especie'];
+    public $with=['raza'];
 
     // Define si la llave primaria es o no un numero autoincrementable 
 
@@ -28,12 +29,18 @@ class Mascota extends Model
         'peso',
         'genero',
         'id_especie',
-        'id_propietario'
+        'id_propietario',
+        'id_raza'
     ];
     //relacion belongsTo
     // Relacion Uno a Uno
     public function especie()
     {
         return $this->belongsTo(Especie::class, 'id_especie', 'id_especie');
+    }
+    public function raza(){
+        return $this->belongsTo(
+        Raza::class, 'id_raza',
+        'id_raza');
     }
 }
